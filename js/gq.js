7c36707628411582c50c2ad0c2c18d4b879a0a35
jeffmeng <jkmexpress@yahoo.com>
2016-09-16 21:55:32 +0000

    bug fix

---
+++ js/gq.js
@@ -0,0 +1,10 @@
+/* copyright 2016 Jeffrey Meng | MIT License | https://tldrlegal.com/license/mit-license#fulltext */
+function getQuery(name) {
+    var  url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
